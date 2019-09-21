1a7df22ca3a10d9aeb40b3c2906796470c383d7b
LiliyaChuvachova <babak.liliya@mail.ru>
2019-09-21 14:42:34 +0300

    add line

--- table.js
+++ table.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('#add').click(function () {
-    $('table').append('<tr><td class="num"></td>,<td class="name"></td>,<td class="patronymic"></td>,<td class="surname"></td>,<td class="del"><button class="remove">Delete</button></td></tr>');
+    $('table').append('<tr><td></td>,<td></td>,<td></td>,<td></td>,<td class="del"><button class="remove">Delete</button></td></tr>');
   });
   $('table').on('click', '.remove', function () {
     $(this).parent().parent().remove();
@@ -8,16 +8,13 @@ $(document).ready(function () {
 
   var n = 0;
   $('#add').click(function () {
-    n++;
-    $('.num').append(n);
-  });
-
-  $('#add').click(function () {
     var name_input = $('input.name_input').val();
     var patronymic_input = $('input.patronymic_input').val();
     var surname_input = $('input.surname_input').val();
-    $('.name').append(name_input);
-    $('.patronymic').append(patronymic_input);
-    $('.surname').append(surname_input);
+    $('tr:last td:nth-child(2)').append(name_input);
+    $('tr:last td:nth-child(3)').append(patronymic_input);
+    $('tr:last td:nth-child(4)').append(surname_input);
+    n++;
+    $('tr:last td:first').prepend(n);
   });
 });
